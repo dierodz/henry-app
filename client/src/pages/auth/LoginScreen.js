adc5b9c7633db813218741afb18c3ec47f4a5e5b
Ernesto Gonzalez Llano <ernestogonzalezllano@gmail.com>
2020-09-30 15:05:49 -0300

    add Login page

--- client/src/pages/auth/LoginScreen.js
+++ client/src/pages/auth/LoginScreen.js
@@ -1,7 +1,171 @@
-import React from "react";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
+import { useFormik } from "formik";
 
-const LoginScreen = () => {
-  return <div>LOGIN</div>;
-};
 
-export default LoginScreen;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  icons:{
+    margin:theme.spacing(3, 0, 2)
+  }
+}));
+
+export default function LoginScreen() {
+  const classes = useStyles();
+  
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate() {
+      const errors = {};
+      if (formik.touched.email && !formik.values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formik.values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+      if (formik.touched.password && !formik.values.password) {
+        errors.password = "Required";
+      } else if (formik.values.password.length <= 8) {
+        errors.password = "Must be more than 8 characters";
+      }
+      return errors;},
+    onSubmit: async (values) => {
+      alert(values.email + values.password)
+      //descomentar al conectar
+
+      /* try {
+        await loginWithEmail(values.email, values.password);
+        history.push("/");
+      } catch { } */
+    }
+  });
+
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={formik.handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={formik.handleChange}
+            error={formik.errors.email?true:false}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={formik.handleChange}
+            error={formik.errors.password?true:false}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Recordarme"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Ingresar
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="/reset" variant="body2">
+                Olvidaste tu contraseña?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/auth/register" variant="body2">
+                No tienes cuenta? Registrate!
+              </Link>
+            </Grid>
+          </Grid>
+          <Grid 
+            container
+            direction="row"
+            justify="space-around"
+            alignItems="center">
+            <Grid 
+              item
+              className={classes.icons}
+              >
+                <IconButton onClick={()=>alert("facebook")} color="primary" aria-label="add to shopping cart">
+                  <Icon className="fab fa-facebook" aria-hidden="true" />
+                </IconButton>
+                <IconButton onClick={()=>alert("Google")} color="primary">
+                  <Icon className="fab fa-google" aria-hidden="true" />
+                </IconButton>
+            </Grid>
+           </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+          <Link color="inherit" href="https://www.soyhenry.com/">
+            Henry
+          </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+      </Box>
+    </Container>
+  );
+}
