9ec1af1e939ad2239d95933ce25103339f091b26
valenfontana7 <valenfontana7@gmail.com>
2020-10-05 22:45:27 -0300

    Tabla generica lista

--- client/src/pages/admin/Cohortes.js
+++ client/src/pages/admin/Cohortes.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import { useQuery } from '@apollo/client';
-import { TabCohortes } from 'components/TabCohortes';
-import COHORTES from 'apollo/querys/cohortes'
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { Tabla } from "components/Tabla";
+import COHORTES from "apollo/querys/cohortes";
 
 function Cohortes({ className }) {
-  const { loading, error, data } = useQuery(COHORTES);
-  return (
-    <div className={className}>
-      <TabCohortes data={data} />
-    </div>
-  )
+   const { loading, error, data } = useQuery(COHORTES);
+   const columnas = [
+      "Nombre de cohorte",
+      "Nombre del instructor",
+      "Cant. de Alumnos",
+   ];
+   const info = [
+      { name: "WEBFT01", instructor: "Toni", cantidad: 194 },
+      { name: "WEBFT02", instructor: "Franco", cantidad: 159 },
+      { name: "WEBFT03", instructor: "Emi", cantidad: 100 },
+   ];
+   return (
+      <div className={className}>
+         <Tabla columnas={columnas} info={info} />
+      </div>
+   );
 }
 
-export default Cohortes
+export default Cohortes;
