02d3521042b242a5328b96378ebddc99548ad6c9
Diego Rodriguez <dierodz91@gmail.com>
2020-10-08 16:25:08 -0300

    fix cohorte resolver

--- client/src/apollo/Mutations/cohortes.js
+++ client/src/apollo/Mutations/cohortes.js
@@ -1,43 +1,47 @@
 import { gql } from '@apollo/client';
 
 export const CREATE_COHORTE = gql`
-mutation createCohorte(
-   $name: String
-   $startDate: String
-   $instructor: Int
- ) {
-   createCohorte(
-     name: $name
-     startDate: $startDate
-     instructor: $instructor
-   ) {
-     id
-   }
- }
- `;
+  mutation createCohorte(
+    $name: String
+    $startDate: String
+    $instructor: Int
+  ) {
+    createCohorte(
+    input: {
+      name: $name
+      startDate: $startDate
+      instructor: $instructor
+    }
+    ) {
+      id
+    }
+  }
+`;
 
 export const DELETE_COHORTE = gql`
- mutation deleteCohorte($id: Int) {
-   deleteCohorte(id: $id) {
-     message
-   }
- }
- `;
+  mutation deleteCohorte($id: Int) {
+    deleteCohorte(id: $id) {
+      message
+    }
+  }
+`;
 
 export const EDIT_COHORTE = gql`
- mutation editCohorte(
-   $id: Int
-   $name: String
-   $startDate: String
-   $instructor: Int
- ) {
-   editCohorte(
-     id: $id
-     name: $name
-     startDate: $startDate
-     instructor: $instructor
-   ) {
-     id
-   }
- }
-  `;
+  mutation editCohorte(
+    $id: Int
+    $name: String
+    $startDate: String
+    $instructor: Int
+  ) {
+    editCohorte(
+      input: {
+        id: $id
+        name: $name
+        startDate: $startDate
+        instructor: $instructor
+      }
+    ) {
+      id
+    }
+  }
+`;
