4ac6a27c8964c02a8bb09ed05d78f805e0dad7f5
Aredex <pacuestar@gmail.com>
2020-10-13 11:56:46 -0500

    ya en el front funciona el poner el role pm

--- client/src/pages/admin/PM.js
+++ client/src/pages/admin/PM.js
@@ -4,63 +4,61 @@ import { Tabla } from "components/Tabla";
 import { getUserRol } from "apollo/querys/users";
 import { ADD_ROLE } from "apollo/Mutations/role";
 
-
 function PM({ className }) {
+  const { loading, error, data, refetch } = useQuery(getUserRol, {
+    variables: { role: "pm" },
+  });
+  const [addRoleMutation, resultAddRole] = useMutation(ADD_ROLE);
 
-   const { loading, error, data, refetch } = useQuery(getUserRol, {
-      variables: { role: "pm" },
-   });
-   const [addRoleMutation, resultAddRole] = useMutation(ADD_ROLE);
+  useEffect(() => {
+    if (!resultAddRole.loading && resultAddRole.called) {
+      refetch();
+    }
+  }, [resultAddRole, refetch]);
 
-   useEffect(() => {
-      if (!resultAddRole.loading && resultAddRole.called) {
-         refetch()
-      }
-   }, [resultAddRole, refetch])
-
-
-   const tableData = useMemo(() => ({
+  const tableData = useMemo(
+    () => ({
       loading,
       error,
       data: data ? data.getUserRol : undefined,
       columns: [
-         { key: 'givenName', label: 'Nombre', align: 'left' },
-         { key: 'familyName', label: 'Apellido', align: 'left' },
-         { key: 'cohorte', label: 'Cohorte', align: 'left' },
-         { key: 'group', label: 'Grupo', align: 'left' },
+        { key: "givenName", label: "Nombre", align: "left" },
+        { key: "familyName", label: "Apellido", align: "left" },
+        { key: "cohorte", label: "Cohorte", align: "left" },
+        { key: "group", label: "Grupo", align: "left" },
       ],
-      addButtonLabel: 'Agregar PM',
+      addButtonLabel: "Agregar PM",
       actions: {
-         create: {
-            initialValues: {
-               email: undefined,
-            },
-            inputs: [
-               { key: 'email', label: "Email" },
-            ],
-            onSubmit: async (values) => {
-               const data = {
-                  variables: {
-                     ...values,
-                     role: 'pm'
-                  }
-               }
-               await addRoleMutation(data)
-            },
-            submitButtonLabel: 'Añadir',
-            title: 'Añadir PM'
-         },
-         delete: {
-            onSubmit: (id) => alert(id)
-         }
-      }
-   }), [data, error, loading]);
+        create: {
+          initialValues: {
+            email: undefined,
+          },
+          inputs: [{ key: "email", label: "Email" }],
+          onSubmit: async (values) => {
+            const data = {
+              variables: {
+                ...values,
+                roleName: "pm",
+              },
+            };
+            await addRoleMutation(data);
+          },
+          submitButtonLabel: "Añadir",
+          title: "Añadir PM",
+        },
+        delete: {
+          onSubmit: (id) => alert(id),
+        },
+      },
+    }),
+    [addRoleMutation, data, error, loading]
+  );
 
-   return (
-      <div className={className}>
-         <Tabla data={tableData} />
-      </div>
-   );
+  return (
+    <div className={className}>
+      <Tabla data={tableData} />
+    </div>
+  );
 }
 
 export default PM;
