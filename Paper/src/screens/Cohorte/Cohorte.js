5655f3c77dd26ce49b900e8dca4592619bf5e7fa
Diego Rodriguez <dierodz91@gmail.com>
2020-10-23 17:06:09 -0300

    connect

--- Paper/src/screens/Cohorte/Cohorte.js
+++ Paper/src/screens/Cohorte/Cohorte.js
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { View, StyleSheet } from "react-native";
-import { Text,IconButton,  Card, Title, Avatar, TextInput} from "react-native-paper";
+import { Text, Card, Title, Avatar } from "react-native-paper";
 import { useSelector } from "react-redux";
-import Hyperlink from 'react-native-hyperlink'
+import Hyperlink from "react-native-hyperlink";
 
 const styles = StyleSheet.create({
   title: {
     textTransform: "capitalize",
-  }})
+  },
+});
 
-
-export default function Cohorte({ navigation }) {
+export default function Cohorte() {
   const { user } = useSelector((state) => state.auth);
 
   return (
@@ -26,12 +26,19 @@ export default function Cohorte({ navigation }) {
           style={styles.title}
           title={user.givenName + " " + user.familyName}
           subtitle={user.nickName}
-          left={(props) => <Avatar.Image {...props} source={{uri:user.photoUrl}} />}
+          left={(props) => (
+            <Avatar.Image {...props} source={{ uri: user.photoUrl }} />
+          )}
         />
         <Card.Content>
           <Title>Libreria de React-native</Title>
-          <Hyperlink linkDefault={ true } linkStyle={ { color: '#2980b9', fontSize: 16 } }>
-           <Text>Les paso la libreria de react native: https://reactnative.dev/</Text>
+          <Hyperlink
+            linkDefault={true}
+            linkStyle={{ color: "#2980b9", fontSize: 16 }}
+          >
+            <Text>
+              Les paso la libreria de react native: https://reactnative.dev/
+            </Text>
           </Hyperlink>
         </Card.Content>
       </Card>
