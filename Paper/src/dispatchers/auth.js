36c640f1745c5c26418acbc5bd9e29bb5269bae7
Diego Rodriguez <dierodz91@gmail.com>
2020-10-26 13:18:41 -0300

    fix

--- Paper/src/dispatchers/auth.js
+++ Paper/src/dispatchers/auth.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 import AsyncStorage from "@react-native-community/async-storage";
 import { authSetError, login, logout } from "../actions/auth";
 
-import { REACT_APP_API } from "@env";
+import { REACT_APP_HTTP } from "@env";
 import { finishLoading, startLoading } from "../actions/ui";
 
 export const signOut = () => {
@@ -15,7 +15,7 @@ export const signOut = () => {
 };
 
 export const initialize = (localUser) => {
-  Axios.defaults.baseURL = REACT_APP_API;
+  Axios.defaults.baseURL = REACT_APP_HTTP;
 
   if (localUser && localUser.token) {
     Axios.defaults.headers.common[
@@ -30,7 +30,7 @@ export const signInWithEmail = (username, password) => {
   return async (dispatch) => {
     dispatch(startLoading());
     try {
-      const { data } = await Axios.post(`${REACT_APP_API}/auth/email`, {
+      const { data } = await Axios.post(`${REACT_APP_HTTP}/auth/email`, {
         username,
         password,
       });
@@ -53,7 +53,7 @@ export const signInWithToken = (token) => {
   return async (dispatch) => {
     dispatch(startLoading());
     try {
-      const { data } = await Axios.get(`${REACT_APP_API}/auth/me`, {
+      const { data } = await Axios.get(`${REACT_APP_HTTP}/auth/me`, {
         headers: { Authorization: `Bearer ${token}` },
       });
       if (data) {
@@ -71,12 +71,12 @@ export const signInWithToken = (token) => {
 
 export const signInWithGoogle = () => {
   if (window) {
-    window.location = `${REACT_APP_API}/auth/google`;
+    window.location = `${REACT_APP_HTTP}/auth/google`;
   }
 };
 
 export const signInWithGithub = () => {
   if (window) {
-    window.location = `${REACT_APP_API}/auth/github`;
+    window.location = `${REACT_APP_HTTP}/auth/github`;
   }
 };
--- Paper/src/screens/General/General.js
+++ Paper/src/screens/General/General.js
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { View,FlatList } from "react-native";
-import { IconButton, TextInput} from "react-native-paper";
+import { View, FlatList } from "react-native";
+import { IconButton, TextInput } from "react-native-paper";
 import { useSelector } from "react-redux";
-import PostCard from "../../components/PostCard/PostCard"
-import { gql, useQuery } from "@apollo/client"
+import PostCard from "../../components/PostCard/PostCard";
+import { gql, useQuery } from "@apollo/client";
 import Loading from "../../components/Loading/Loading";
 
-const subscribePost= gql`
+const subscribePost = gql`
   subscription {
     subscribePost {
       id
@@ -14,76 +14,83 @@ const subscribePost= gql`
       content
     }
   }
-`
-const getPost = gql`
-query  {
-  getPost {
-    id
-    title
-    content
+`;
+const GET_POST = gql`
+  query {
+    getPost {
+      id
+      tittle
+      content
+    }
   }
-}
-`
+`;
 export default function General(props) {
   const { user } = useSelector((state) => state.auth);
-  const [text, setText] = React.useState('');
-  const {data:{getPost}, loading, error}= useQuery(getPost)
+  const [text, setText] = React.useState("");
+  const { data: preData, loading, error } = useQuery(GET_POST);
 
-  const data = React.useMemo(()=>{
-    if(getPost){
-      return getPost.map((post)=>({id:post.id,
-        name:user.givenName + " " + user.familyName,
-      nickName: user.nickName,
-      photoUrl: user.photoUrl,
-      title: post.title,
-      content: post.content
-      }))
+  const data = React.useMemo(() => {
+    if (preData) {
+      return preData.getPost.map((post) => ({
+        id: post.id,
+        name: user.givenName + " " + user.familyName,
+        nickName: user.nickName,
+        photoUrl: user.photoUrl,
+        title: post.tittle,
+        content: post.content,
+      }));
     }
-    return undefined
-  },[getPost])
+    return undefined;
+  }, [preData]);
 
- 
-  return (loading? <Loading/>
-  :
-  <>
-    <View
-      style={{
-        flex: 1,
-        padding: 20,
-        alignItems: "center",
-      }}
-    >
-      <FlatList
-      style={{width:"100%"}}
-              data={data} 
-              renderItem={PostCard}
-              keyExtractor={(props) => {
-                  return props.id.toString()
-                 }}
-            />  
-    </View>
-    <View style={{ width:"100%", bottom:0,flexDirection:"row" } } >
-      <TextInput
-          style={{ width:"100%", bottom:0, flex:1, } } 
-        label="Posteá!"
-        placeholder="Posteá!"
-        value={text}
-        onChangeText={text => setText(text)}
-        right={<TextInput.Icon name={() =><IconButton
-          style={{alignSelf:"center"} } 
-          width="100%"
-          icon="send"
-          size={20}
-          onPress={() => {alert(text)
-            setText('')
+  return loading ? (
+    <Loading />
+  ) : (
+    <>
+      <View
+        style={{
+          flex: 1,
+          padding: 20,
+          alignItems: "center",
+        }}
+      >
+        <FlatList
+          style={{ width: "100%" }}
+          data={data}
+          renderItem={PostCard}
+          keyExtractor={(props) => {
+            return props.id.toString();
           }}
-          />}/>}
-      />
-    </View>
+        />
+      </View>
+      <View style={{ width: "100%", bottom: 0, flexDirection: "row" }}>
+        <TextInput
+          style={{ width: "100%", bottom: 0, flex: 1 }}
+          label="Posteá!"
+          placeholder="Posteá!"
+          value={text}
+          onChangeText={(text) => setText(text)}
+          right={
+            <TextInput.Icon
+              name={() => (
+                <IconButton
+                  style={{ alignSelf: "center" }}
+                  width="100%"
+                  icon="send"
+                  size={20}
+                  onPress={() => {
+                    alert(text);
+                    setText("");
+                  }}
+                />
+              )}
+            />
+          }
+        />
+      </View>
     </>
   );
 }
-
 
 /* const data=props.route.params.screen=="cohorte"? [{id:1,
   name:user.givenName + " " + user.familyName,
